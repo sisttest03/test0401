eae54cc5dcbfaa8abf9fafc844bce83dba30b025
sisttest03 <sisttest03@gmail.com>
2021-04-01 14:45:37 +0900

    create project

---
+++ test0401/src/main/java/com/sist/goods/Test0401Application.java
@@ -0,0 +1,13 @@
+package com.sist.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test0401Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test0401Application.class, args);
+	}
+
+}
---
+++ test0401/src/test/java/com/sist/goods/Test0401ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sist.goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test0401ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
